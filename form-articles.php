638cd8d1c0c4cff0c1d1fa348273d43df01606f8
ThomasBrossier <98547958+ThomasBrossier@users.noreply.github.com>
2023-01-27 12:00:02 +0100

    Solve category bug in artcile edit page

--- form-articles.php
+++ form-articles.php
@@ -12,6 +12,7 @@
             'category'=> '',
             'content'=>''
     ];
+    $category = '';
     if(file_exists($filename)){
         $articles = json_decode(file_get_contents($filename) ,true) ?? [];
     }
