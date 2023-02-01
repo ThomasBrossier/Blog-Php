f36c9cafe112d9bfb1be54c5161bbd92b4f92e37
ThomasBrossier <98547958+ThomasBrossier@users.noreply.github.com>
2023-02-01 12:11:54 +0100

    Change header menu & profile menu

--- src/includes/header.php
+++ src/includes/header.php
@@ -9,12 +9,18 @@ $currentUser = $currentUser ?? false;
         <li class="<?= $_SERVER['REQUEST_URI'] === '/src/form-articles.php' ? 'active' : '' ?>">
             <a href="/src/form-articles.php">Ecrire un article</a>
         </li>
-        <li class="<?= $_SERVER['REQUEST_URI'] === '/src/profile.php' ? 'active' : '' ?>">
-            <a href="/src/profile.php">Ma Page</a>
+        <li class="<?= $_SERVER['REQUEST_URI'] === '/src/profile.php' ? 'active' : '' ?> profile-menu">
+            <span><img alt="avatar" src="/public/images/avatar.webp" /></span>
+            <ul class="sub-menu">
+                <li>
+                    <a href="/src/profile.php">Mon Profil</a>
+                </li>
+                <li>
+                    <a href="/src/auth-logout.php">Déconnexion</a>
+                </li>
+            </ul>
         </li>
-        <li>
-            <a href="/src/auth-logout.php">Déconnexion</a>
-        </li>
+
         <?php else: ?>
         <li class="<?= $_SERVER['REQUEST_URI'] === '/src/auth-register.php' ? 'active' : '' ?>">
             <a href="/src/auth-register.php">Inscription</a>
