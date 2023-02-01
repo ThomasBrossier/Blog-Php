46164ebc9bd8e4f7457dc9c398166a8a65a59035
ThomasBrossier <98547958+ThomasBrossier@users.noreply.github.com>
2023-02-01 16:50:01 +0100

    Add security for Sessions

--- src/database/AuthDB.php
+++ src/database/AuthDB.php
@@ -7,11 +7,12 @@ class AuthDB {
     private PDOStatement $statementUserByID;
     private PDOStatement $statementUserByEmail;
     private PDOStatement $statementRegister;
+    private string $secret = 'MON_CODE_SECRET';
 
     public function __construct(private PDO $pdo){
 
         $this->statementGetSession = $this->pdo->prepare('SELECT * FROM session WHERE id=:id');
-        $this->statementCreateSession = $this->pdo->prepare('INSERT INTO session ( userid ) VALUES (:userid)');
+        $this->statementCreateSession = $this->pdo->prepare('INSERT INTO session ( id,userid ) VALUES (:id,:userid)');
         $this->statementDeleteSession = $this->pdo->prepare('DELETE FROM session WHERE id=:id');
         $this->statementUserByID = $this->pdo->prepare('SELECT * FROM user WHERE id=:id');
         $this->statementUserByEmail = $this->pdo->prepare('SELECT * FROM user WHERE email=:email');
@@ -29,14 +30,21 @@ class AuthDB {
     }
 
     public function login(string $id) :void{
-        $this->statementCreateSession->execute([':userid' => (int)$id ]);
-        $sessionId = $this->pdo->lastInsertId();
+        $sessionId = bin2hex(random_bytes(32));
+        $this->statementCreateSession->execute([
+            ':userid' => (int)$id,
+            ':id' => $sessionId
+        ]);
+        $signature = hash_hmac('sha256',$sessionId, $this->secret);
         setcookie('session', $sessionId, time() + 60*60*24*30,'/','', false, true);
+        setcookie('signature', $signature, time() + 60*60*24*30,'/','', false, true);
     }
+
     public function getUserByEmail(string $email) :array{
         $this->statementUserByEmail->execute([':email' => $email ]);
         return $this->statementUserByEmail->fetch();
     }
+
     public function register(array $credentials) :void{
         $hashedPassword = password_hash($credentials['password'], PASSWORD_ARGON2ID);
         $this->statementRegister->bindValue(':firstname', $credentials['firstname']);
@@ -45,24 +53,30 @@ class AuthDB {
         $this->statementRegister->bindValue(':password', $hashedPassword);
         $this->statementRegister->execute();
     }
+
     public function isLoggedIn() :array | false{
         $sessionId = $_COOKIE['session'] ?? '';
-        if($sessionId){
-
-            $this->statementGetSession->execute([':id'=> (int)$sessionId]);
-            $session = $this->statementGetSession->fetch();
-            if($session){
-                $this->statementUserByID->execute([':id'=> $session['userid']]);
-                $user = $this->statementUserByID->fetch();
+        $signature =  $_COOKIE['signature'] ?? '';
+        if($sessionId && $signature){
+            $hash = hash_hmac('sha256',$sessionId, $this->secret);
+            if(hash_equals($hash, $signature)){
+                $this->statementGetSession->execute([':id'=> $sessionId]);
+                $session = $this->statementGetSession->fetch();
+                if($session){
+                    $this->statementUserByID->execute([':id'=> $session['userid']]);
+                    $user = $this->statementUserByID->fetch();
+                }
             }
         }
         return $user ?? false;
     }
+
     public function logout() : void{
         $sessionId = $_COOKIE['session'] ?? '';
         if($sessionId){
-            $this->statementDeleteSession->execute([':id'=> (int)$sessionId]);
+            $this->statementDeleteSession->execute([':id'=> $sessionId]);
             setcookie('session', '', time()-1 ,'/');
+            setcookie('signature', '', time()-1 ,'/');
         }
     }
 }
